001b210c9d853f11579f6378a2ccda362ee55697
mohamedelleuch-mediamarkt <elleuch@media-saturn.com>
2023-04-19 14:46:39 +0300

    load helpers fixed

--- src/class2Object.js
+++ src/class2Object.js
@@ -2,49 +2,53 @@
 let methodId = 0;
 
 function classToObject(sourceCode, className) {
-    methods2Object = {};
-    methodId = 0;
-    methods2Object.name = className;
-    brutFile = sourceCode;
-    var ss = `^\\s*class\\s*${className}\\s*{\\s*`;
-    ss = new RegExp(ss, "gm");
-    let match = ss.exec(sourceCode)
-    let startPosition = match.index;
-    ss = new RegExp("// ###___END_OF_CLASS___###", "gm");
-    match = ss.exec(sourceCode);
-    let endPosition = match.index;
-    methods2Object.startPosition = startPosition;
-    methods2Object.endPosition = endPosition;
-    let classCode = sourceCode.substring(startPosition,endPosition+4);
-    var re = /[^\t*]\b(?!function\b).*?\b\s*\w*\s*\(\w*\)\s*\{/gm
-    methods2ObjectList = [];
-    while ((match = re.exec(classCode)) != null) {
-        let tempMeth = class2ObjReadMethod(classCode,match);
-        // remove parse function from code:------------------------------
-        let count = tempMeth.endPosition+1 - tempMeth.startPosition;
-        let emptySpace = " ".repeat(count);
-        classCode = classCode.substring(0,tempMeth.startPosition) + emptySpace + classCode.substring(tempMeth.endPosition+1);
-        //---------------------------------------------------------------
-        // remove return char \n and \r from the beginning and the end of the class code ------------------------
-        if(tempMeth.body[0] === '\n' || tempMeth.body[0] === '\r') tempMeth.body = tempMeth.body.substring(1);
-        if(tempMeth.body[tempMeth.body.length-1] === '\n' || tempMeth.body[tempMeth.body.length-1] === '\r') tempMeth.body = tempMeth.body.substring(0, tempMeth.body.length-1);
-        // ------------------------------------------------------------------------------------------------------
-        tempMeth.modified = false;
-        // a method processed: ***********
-        methods2ObjectList.push(tempMeth);
-        //******************************* */
-    }
-    $(window).ready( () => {
-        const helperSelected = (e) => {
-            let txt = e.currentTarget.innerText;
-            insertTextAtCursor("scriptLib."+txt+"(");
+    try {
+        methods2Object = {};
+        methodId = 0;
+        methods2Object.name = className;
+        brutFile = sourceCode;
+        var ss = `^\\s*class\\s*${className}\\s*{\\s*`;
+        ss = new RegExp(ss, "gm");
+        let match = ss.exec(sourceCode)
+        let startPosition = match.index;
+        ss = new RegExp("// ###___END_OF_CLASS___###", "gm");
+        match = ss.exec(sourceCode);
+        let endPosition = match.index;
+        methods2Object.startPosition = startPosition;
+        methods2Object.endPosition = endPosition;
+        let classCode = sourceCode.substring(startPosition,endPosition+4);
+        var re = /[^\t*]\b(?!function\b).*?\b\s*\w*\s*\(\w*\)\s*\{/gm
+        methods2ObjectList = [];
+        while ((match = re.exec(classCode)) != null) {
+            let tempMeth = class2ObjReadMethod(classCode,match);
+            // remove parse function from code:------------------------------
+            let count = tempMeth.endPosition+1 - tempMeth.startPosition;
+            let emptySpace = " ".repeat(count);
+            classCode = classCode.substring(0,tempMeth.startPosition) + emptySpace + classCode.substring(tempMeth.endPosition+1);
+            //---------------------------------------------------------------
+            // remove return char \n and \r from the beginning and the end of the class code ------------------------
+            if(tempMeth.body[0] === '\n' || tempMeth.body[0] === '\r') tempMeth.body = tempMeth.body.substring(1);
+            if(tempMeth.body[tempMeth.body.length-1] === '\n' || tempMeth.body[tempMeth.body.length-1] === '\r') tempMeth.body = tempMeth.body.substring(0, tempMeth.body.length-1);
+            // ------------------------------------------------------------------------------------------------------
+            tempMeth.modified = false;
+            // a method processed: ***********
+            methods2ObjectList.push(tempMeth);
+            //******************************* */
         }
-        window.helperSelected = helperSelected;
-        let listUL = $('#right-toolbar .helpers-list');
-        let listLI = methods2ObjectList.map( i => i.name).sort().map( i => `<li class="list-item" onclick="helperSelected(event)">${i}</li>`).join('\n');
-        listUL.html(listLI);
-    });
-    return methods2ObjectList;
+        $(window).ready( () => {
+            const helperSelected = (e) => {
+                let txt = e.currentTarget.innerText;
+                insertTextAtCursor("scriptLib."+txt+"(");
+            }
+            window.helperSelected = helperSelected;
+            let listUL = $('#right-toolbar .helpers-list');
+            let listLI = methods2ObjectList.map( i => i.name).sort().map( i => `<li class="list-item" onclick="helperSelected(event)">${i}</li>`).join('\n');
+            listUL.html(listLI);
+        });
+        return methods2ObjectList;
+    } catch (err) {
+        return [];
+    }
 }
 
 
